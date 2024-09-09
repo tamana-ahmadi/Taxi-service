b581fb7d8288e6846db3b2154c965c765e530a47
Nabiev Asliddin <aslnabiev2002@gmail.com>
2024-09-09 08:50:06 +0500

    update model orders

--- models/orders.go
+++ models/orders.go
@@ -7,10 +7,10 @@ type Ord struct {
 	ClientID   int       `gorm:"references users(id)" json:"client_id"`
 	DriverID   int       `gorm:"references users(id)" json:"driver_id"`
 	RouteID    int       `gorm:"references routes(id)" json:"route_id"`
-	Distance   int       `gorm:"foreignKey:RouteID" json:"distance"`
-	Price      int       `gorm:"foreignKey:RouteID" json:"price"`
+	Distance   Route     `gorm:"foreignKey:RouteID" json:"distance"`
+	Price      Route     `gorm:"foreignKey:RouteID" json:"price"`
 	TaxicompID int       `gorm:"references taxicompanies(id)" json:"taxicomp_id"`
-	Comptitle  string    `gorm:"foreignKey:TaxicompID" json:"comp_title"`
+	Comptitle  TaxiComp  `gorm:"foreignKey:TaxicompID" json:"comp_title"`
 	IsDone     bool      `gorm:"default false" json:"is_done"`
 	IsDeleted  bool      `gorm:"default false" json:"is_deleted"`
 	CreatedAt  time.Time `json:"created_at" gorm:"autoCreateTime"`
