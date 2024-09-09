bb810f4fee7a43cbfd317d0b990330dd29ce8a95
Nabiev Asliddin <aslnabiev2002@gmail.com>
2024-09-09 10:53:18 +0500

    upd1

--- package/repository/orders.go
+++ package/repository/orders.go
@@ -49,8 +49,7 @@ func CheckOrdersasDone(isdone bool, id int) error {
 }
 func OrdersReport(isdone, isdeletedo, isdeletedt, isdeletedr, isresp, isblocked, isdeletedu bool) ([]models.Order, error) {
 	var order []models.Order
-	ordersreport := `SELECT t.comp_title, o.is_done, COUNT(DISTINCT CASE WHEN u.role='user' THEN u.id END) AS clientid, COUNT(DISTINCT CASE WHEN u.role='driver' THEN u.id END) AS driverid, COUNT(DISTINCT o.route_id) AS routeid, SUM(r.distance) as distance, SUM(r.price) as price FROM orders o LEFT JOIN taxicompanies t ON t.id = o.taxicomp_id LEFT JOIN routes r ON r.id=o.route_id  LEFT JOIN users u ON u.id = o.client_id OR u.id = o.driver_id WHERE o.is_done = ? AND o.is_deleted = ? AND t.is_deleted = ? AND r.is_deleted=? AND r.is_response=?  AND u.is_blocked = ? AND u.is_deleted = ? GROUP BY t.comp_title, o.is_done ORDER BY clientid, driverid DESC`
-	err := db.GetconnectDB().Raw(ordersreport, isdone, isdeletedo, isdeletedt, isdeletedr, isresp, isblocked, isdeletedu).Scan(&order).Error
+	err := db.GetconnectDB().Select("t.comp_title, o.is_done, COUNT(DISTINCT CASE WHEN u.role='user' THEN u.id END) AS clientid, COUNT(DISTINCT CASE WHEN u.role='driver' THEN u.id END) AS driverid, COUNT(DISTINCT o.route_id) AS routeid, SUM(r.distance) as distance, SUM(r.price) as price").Joins("LEFT JOIN taxicompanies t ON t.id = o.taxicomp_id").Joins("LEFT JOIN routes r ON r.id=o.route_id").Joins("LEFT JOIN users u ON u.id = o.client_id OR u.id = o.driver_id").Where("o.is_done = ? AND o.is_deleted = ? AND t.is_deleted = ?", isdone, isdeletedo, isdeletedt).Where("r.is_deleted=? AND r.is_response=?", isdeletedr, isresp).Where("u.is_blocked = ? AND u.is_deleted = ?", isblocked, isdeletedu).Group("t.comp_title, o.is_done").Order("clientid, driverid DESC").Find(&order).Error
 	if err != nil {
 		logger.Error.Printf("[repository.OrdersReport]error in  order report %s\n", err.Error())
 		return order, err
