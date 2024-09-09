dc87816b3cf6aa2af9d637f876abc7486bbf0af2
Nabiev Asliddin <aslnabiev2002@gmail.com>
2024-09-09 17:45:20 +0500

    u

--- package/repository/routes.go
+++ package/repository/routes.go
@@ -30,7 +30,7 @@ func SoftDeleteRoutes(isdeleted bool, id int) error {
 }
 
 func GetAllRoutes(isdeleted, isresp, isblocked bool, price int, uid uint) (route []models.Route, err error) {
-	err = db.GetconnectDB().Preload("User").Joins("Join users ON users.id=routes.user_id").Where("routes.is_response=? AND routes.is_deleted=? AND routes.price=?", isresp, isdeleted, price).Where("routes.user_id=?", uid).Order("routes.id").Where("users.is_blocked=? AND users.is_deleted=?", isblocked, isdeleted).Find(&route).Error
+	err = db.GetconnectDB().Preload("User").Joins("Join users ON users.id=routes.user_id OR users.id=routes.driver_id").Where("routes.is_response=? AND routes.is_deleted=? AND routes.price=?", isresp, isdeleted, price).Where("routes.user_id=?", uid).Order("routes.id").Where("users.is_blocked=? AND users.is_deleted=?", isblocked, isdeleted).Find(&route).Error
 	if err != nil {
 		logger.Error.Printf("[repository.GetAllRoutes]error in getting all orders %s\n", err.Error())
 		return route, err
@@ -38,7 +38,7 @@ func GetAllRoutes(isdeleted, isresp, isblocked bool, price int, uid uint) (route
 	return route, nil
 }
 func GetAllRoutesByID(isdeleted, isblocked bool, uid, rid uint) (route []models.Route, err error) {
-	err = db.GetconnectDB().Preload("User").Joins("Join users ON users.id=routes.client_id").Where("routes.is_deleted=?", isdeleted).Where("routes.id=?  AND routes.client_id=?", rid, uid).Order("routes.id").Where("users.is_blocked=? AND users.is_deleted=?", isblocked, isdeleted).Find(&route).Error
+	err = db.GetconnectDB().Preload("User").Joins("Join users ON users.id=routes.client_id OR users.id=routes.driver_id ").Where("routes.is_deleted=?", isdeleted).Where("routes.id=?  AND routes.client_id=?", rid, uid).Order("routes.id").Where("users.is_blocked=? AND users.is_deleted=?", isblocked, isdeleted).Find(&route).Error
 	if err != nil {
 		logger.Error.Printf("[repository.GetAllRoutesByID]error in getting all order by id %s\n", err.Error())
 		return route, err
