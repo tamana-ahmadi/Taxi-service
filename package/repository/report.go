76f0aee67a263589fd52bca84855b9b786d51d95
Nabiev Asliddin <aslnabiev2002@gmail.com>
2024-09-16 23:12:57 +0500

    start

--- package/repository/report.go
+++ package/repository/report.go
@@ -11,7 +11,7 @@ func OrdersReport(isresp, isdeletedr, isblocku, isdeletedu bool) (report []model
 	COUNT(DISTINCT CASE WHEN users.role='user' THEN users.id END) as client_id,
 	COUNT(DISTINCT CASE WHEN users.role='driver' THEN users.id END) as driver_id,
 	SUM(routes.distance*routes.price) as incomes FROM routes, users WHERE users.id=routes.driver_id AND routes.is_response=? AND routes.is_deleted=?
-	AND users.is_blocked=? AND users.is_deleted=? GROUP BY from,into,is_response ORDER BY incomes DESC`, isresp, isdeletedr, isblocku, isdeletedu).Scan(&report).Error
+	AND users.is_blocked=? AND users.is_deleted=? GROUP BY routes.from,routes.into,routes.is_response ORDER BY incomes DESC`, isresp, isdeletedr, isblocku, isdeletedu).Scan(&report).Error
 	if err != nil {
 		logger.Error.Printf("[repository.OrdersReport]error in  orders report %s\n", err.Error())
 		return report, err
