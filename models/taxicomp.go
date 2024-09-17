dbf28b3327a6f98bfea92da8b6704d75dcc022a1
Nabiev Asliddin <aslnabiev2002@gmail.com>
2024-09-17 09:08:01 +0500

    add change

--- models/taxicomp.go
+++ models/taxicomp.go
@@ -6,21 +6,12 @@ type TaxiComp struct {
 	ID        int       `gorm:"primary key" json:"-"`
 	CompTitle string    `gorm:"not null" json:"company_title"`
 	IsDeleted bool      `gorm:"default false" json:"-"`
-	DriverID  int       `json:"driver_id"`
-	Driver    User      `gorm:"references users(id);foreignKey:DriverID" json:"-"`
+	DriverID  int       `gorm:"references users(id)" json:"driver_id"`
+	Driver    User      `gorm:"foreignKey:DriverID"  json:"driver`
 	CreatedAt time.Time `json:"-" gorm:"autoCreateTime"`
 	UpdatedAt time.Time `json:"-" gorm:"autoUpdateTime"`
 }
 
 func (TaxiComp) TableName() string {
 	return "taxicompanies"
-}
-
-type TaxiCompanies struct {
-	CompTitle string    `gorm:"not null" json:"company_title"`
-	IsDeleted bool      `gorm:"default false" json:"-"`
-	DriverID  int       `json:"driver_id"`
-	Driver    User      `gorm:"references users(id);foreignKey:DriverID" json:"driver"`
-	CreatedAt time.Time `json:"-" gorm:"autoCreateTime"`
-	UpdatedAt time.Time `json:"-" gorm:"autoUpdateTime"`
 }
--- package/repository/taxicomp.go
+++ package/repository/taxicomp.go
@@ -31,7 +31,7 @@ func SoftDeleteTaxicomps(isdeleted bool, id int) error {
 }
 
 func GetAllTaxicomps(isdeletedt, isblocked, isdeletedu bool) (txcm []models.TaxiComp, err error) {
-	err = db.GetconnectDB().Preload("User").Joins("Join users ON users.id=taxicompanies.driver_id").Where("taxicompanies.is_deleted=?", isdeletedt).Where("users.is_blocked=? AND users.is_deleted=?", isblocked, isdeletedu).Find(&txcm).Error
+	err = db.GetconnectDB().Joins("Join users ON users.id=taxicompanies.driver_id").Where("taxicompanies.is_deleted=?", isdeletedt).Where("users.is_blocked=? AND users.is_deleted=?", isblocked, isdeletedu).Find(&txcm).Error
 	if err != nil {
 		logger.Error.Printf("[repository.getalltaxicomps]error in getting all taxi companies %s\n", err.Error())
 		return txcm, err
