087f3ba7ff7d4e8b29ec2b145a76ed1038577159
Nabiev Asliddin <aslnabiev2002@gmail.com>
2024-09-20 01:07:46 +0500

    run

--- package/repository/routes.go
+++ package/repository/routes.go
@@ -30,7 +30,7 @@ func SoftDeleteRoutes(isdeleted bool, id int) error {
 }
 
 func Report(isresp, isdeletedr, isblocked, isdeletedu bool, price int) (route []models.GetRoutes, err error) {
-	err = db.GetconnectDB().Raw("Select r.from, r.into, r.is_response, SUM(DISTINCT r.distance) as distance ,SUM(DISTINCT r.pricekm) as pricekm, SUM(r.all_price/2) as all_price, COUNT(DISTINCT CASE WHEN u.role='user' THEN u.id END) as client_id, COUNT(DISTINCT CASE WHEN u.role='driver' THEN u.id END) as driver_id FROM routes r, users u Where r.client_id=u.id OR r.driver_id=u.id  AND  r.is_response=? AND r.is_deleted=?  AND u.is_blocked=? AND u.is_deleted=? AND all_price<=? GROUP BY t.comp_title, r.from,r.into,r.is_response ORDER BY all_price DESC", isresp, isdeletedr, isblocked, isdeletedu, price).Scan(&route).Error
+	err = db.GetconnectDB().Raw("Select r.from, r.into, r.is_response, SUM(DISTINCT r.distance) as distance ,SUM(DISTINCT r.pricekm) as pricekm, SUM(r.all_price/2) as all_price, COUNT(DISTINCT CASE WHEN u.role='user' THEN u.id END) as client_id, COUNT(DISTINCT CASE WHEN u.role='driver' THEN u.id END) as driver_id FROM routes r, users u Where r.client_id=u.id OR r.driver_id=u.id  AND  r.is_response=? AND r.is_deleted=?  AND u.is_blocked=? AND u.is_deleted=? AND all_price<=? GROUP BY  r.from,r.into,r.is_response ORDER BY all_price DESC", isresp, isdeletedr, isblocked, isdeletedu, price).Scan(&route).Error
 	if err != nil {
 		logger.Error.Printf("[repository.GetAllRoutes]error in report %s\n", err.Error())
 		return route, err
